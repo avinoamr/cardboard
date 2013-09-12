c4c56053a55de2931a25c2655fbbe472e41991b5
Roi Avinoam <avinoamr@gmail.com>
2013-09-12 22:56:03 +0300

    Basic structure

--- jsongrid.js
+++ jsongrid.js
@@ -3,13 +3,21 @@
     // grid constructor
     var Grid = function( el, data, options ) {
         this.el = $( el );
+        console.log( options );
         $.extend( this, options );
         this.data = data;
     };
 
     // render the grid
     Grid.prototype.render = function() {
-        this.el.html( this.template );
+        this.el.html( "<table class='grid'><tbody></tbody></table>" );
+
+        if ( this.title ) {
+            this.render_item({ 
+                key: this.title,
+                pad: false
+            })
+        }
 
         for ( var prop in this.data ) {
             this.render_item({ 
@@ -26,56 +34,57 @@
         return this.el.find.apply( this.el, arguments );
     };
 
+    // render the left pad and selection box
+    Grid.prototype.render_pad = function( item, to ) {
+        var pad = $( "<td class='pad' />" );
+        pad.html( "<input type='checkbox' />" );
+        to.append( pad );
+        return this;
+    };
+
+    // render the key
+    Grid.prototype.render_key = function( item, to ) {
+        var key = $( "<td class='key' />" );
+        key.html( item.key );
+        if ( item.pad === false ) {
+            key.attr( "colspan", 2 );
+        }
+        to.append( key );
+        return this;
+    };
+
+    // render the value
+    Grid.prototype.render_value = function( item, to ) {
+        var value = $( "<td class='value' />" );
+        value.html( item.value );
+        to.append( value );
+        return this;
+    };
+
+    // render nested object
+    Grid.prototype.render_nested = function( item, to ) {
+        var nested = $( "<td class='nested' colspan='2' />" );
+        to.append( nested );
+        nested.jsongrid( item.value, { title: item.key } );
+        return this;
+    };
 
     // render a specific item
     Grid.prototype.render_item = function( item ) {
-        var row = $( this.template_item );
-        this.$( "table.jsongrid" ).append( row );
+        var row = $( "<tr />" );
+        this.$( "table.grid" ).append( row );
 
-        // pad
-        var pad = row.find( "> .pad" );
-        pad.width( 30 );
-        pad.css( "background", "#f5f5f5" );
-        if ( this.selectable ) {
-            pad.html( "<input type='checkbox' />" );
+        if ( item.pad !== false ) {
+            this.render_pad( item, row );
         }
 
-        // nested object
         if ( "object" == typeof item.value ) {
-            row.find( "> .value" ).remove();
-            row.find( "> .key" )
-                .attr( "colspan", 2 )
-                .css( "padding", 0 )
-                .css( "border", "none" )
-                .jsongrid( item.value );
-            return this;
+            this.render_nested( item, row );
+        } else {
+            this.render_key( item, row )
+                .render_value( item, row );
         }
 
-        // value
-        var itemvalue = item.value, nested = false;
-        if ( "object" == typeof itemvalue ) {
-            itemvalue = JSON.stringify( itemvalue );
-            nested = true;
-        }
-        var value = row.find( "> .value" );
-        value.width( 720 );
-        value.html( itemvalue );
-
-        // key
-        var key = row.find( "> .key" );
-        var itemkey = $( "<span />" ).html( item.key );
-        if ( nested ) {
-            var carret = $( "<a href='#' class='glyphicon glyphicon-chevron-right'></a>" );
-            carret.css( "padding-right", 10 );
-            key.append( carret )
-        }
-        key.append( itemkey );
-
-        
-        
-
-
-        
         return this;
     };
 
@@ -92,10 +101,7 @@
 
     // default options
     $.fn.jsongrid.defaults = {
-        template: "<table class='jsongrid'></table>",
-        template_item: "<tr><td class='pad'></td><td class='key'></td><td class='value'></td></tr>",
-        Grid: Grid,
-        selectable: true
+        Grid: Grid
     };
 
 }( jQuery ))
